759e62d53285ffac2205a4e4c4560a5beb6898d2
Bert Spaan <bert.spaan@gmail.com>
2020-05-12 21:18:08 +0200

    added about route

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import InfiniteLoading from 'vue-infinite-loading'
 
 Vue.use(VueRouter)
+Vue.use(InfiniteLoading, { /* options */ })
 
 Vue.config.productionTip = false
 
-const router = new VueRouter()
+const router = new VueRouter({
+  routes: [
+    {
+      name: 'main',
+      path: '/',
+      component: App
+    },
+    {
+      name: 'about',
+      path: '/about',
+      component: App
+    }
+  ]
+})
 
 new Vue({
   render: h => h(App),
