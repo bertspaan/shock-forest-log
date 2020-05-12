05c047e5123bae7e6277ed27f176b4f5e467de70
Bert Spaan <bert.spaan@gmail.com>
2020-05-12 21:06:50 +0200

    load static json files instead of API

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const API_URL = process.env.VUE_APP_API_URL
+const API_EXTENSION = process.env.VUE_APP_API_EXTENSION
 
 export default async function fetch (path) {
-  const response = await axios.get(`${API_URL}${path}`)
+  const response = await axios.get(`${API_URL}${path}${API_EXTENSION}`)
   return response.data
 }
