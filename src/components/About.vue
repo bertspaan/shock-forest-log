3e57b56ed5f534ac3813120885e46b58eefda970
Bert Spaan <bert.spaan@gmail.com>
2020-05-12 20:50:06 +0200

    added About page

---
+++ src/components/About.vue
@@ -0,0 +1,68 @@
+<template>
+  <section>
+    <p>
+      From September 11, 2019 through December 21, 2019, a group of researchers, artists, and
+      activists called the <L t="shockforestgroup" /> lived and worked at <L t="hethem" />,
+      a cultural institution in Zaandam, Netherlands. Het HEM is located in the former 0,50 caliber
+      <L t="bullets" /> factory on the ex-military <L t="hembrug" /> terrain. Although
+      the building was built in 1956, the company that occupied it, Artillerie Inrichtingen,
+      also known as <L t="ai" />, had been supplying weapons
+      for Dutch <L t="colonialism" /> since 1679. In 1973, AI split, and the 0,50 factory
+      became property of the new company <L t="eurometaal" />.
+    </p>
+    <p>
+      EuroMetaal continued to use the Shock Forest for <L t="weapons" /> and ammunition testing.
+      The Forest was specifically designed and planted to withstand <L t="explosions" />. For half
+      a century, it both insulated the <L t="sound" /> of explosive tests and hid military activity
+      from view. By studying the terrain, excavating the man-made myths and clay histories that
+      make up the Forest’s foundation, we have hoped to give voice to the <L t="trees" />,
+      <L t="waves" />, <L t="soil" /> and <L t="herons" /> that have lived
+      alongside <L t="industrialization" /> and <L t="militarization" /> in the terrain for decades.
+    </p>
+    <p>
+      On November 21, 2019, the day before the 340th anniversary of EuroMetaal, our <L t="exhibition" />
+      No Camouflage opened at Het HEM. The exhibition was a milestone in our artistic research to
+      uncover the truth behind the <L t="opacity" /> of what was on this terrain and what is. It’s an
+      investigation into the <L t="duality" /> that emerges over <L t="time" />,
+      into <L t="monument" /> and <L t="legality" /> as a paternalistic form of environmental
+      protection, into <L t="activism" /> and <L t="labor" /> and how they might shape our society today.
+    </p>
+    <p>
+      Our work has been an ongoing experiment in presence. If traditional research works in a
+      manner akin to traditional music concerts, with categories defined a priori,
+      <L t="scores" /> set with pre-ordered patterns and clear end goals, we work more like a
+      free <L t="dance" /> of improvisation, excavating a polyphony of truths, and listening
+      to the <L t="reverb" /> resonate within and through <L t="traces" /> of the building’s past.
+    </p>
+    <p>
+      Immersion into this hyperlocal environment has opened our eyes to what it means to work in
+      a collective, to allow for <L t="cycles" /> of emergence in former spaces of monodirectional
+      production. We have shown that our process is not only a part of our ongoing work, but also
+      its essence. We have let our voices emerge organically, and we have seen them grow both
+      collectively and individually. We have sought to learn how to incorporate ourselves with
+      the <L t="light" /> and <L t="violence" /> that surrounds us. We hope that those who
+      follow in our footsteps will do the same.
+    </p>
+    <p>
+      Explore this <L t="log" /> by clicking on hashtags that interest you. See how the themes
+      interconnect. Ask yourself what it means to be situated in a place that makes so many
+      ripples across global theaters of war. What stories lay camouflaged here?
+      Where does the bullet land?
+    </p>
+    <router-link :to="{name: 'main'}" tag="button">Explore the Shock Forest Log</router-link>
+  </section>
+</template>
+
+<script>
+import L from './HashtagLink.vue'
+
+export default {
+  name: 'about',
+  components: {
+    L
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/HashtagLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link :to="{name: 'main', query: {hashtags: t}}">#{{ t }}</router-link>
+</template>
+
+<script>
+export default {
+  name: 'hashtag-link',
+  props: {
+    t: String
+  }
+}
+</script>
+
+<style>
+</style>
